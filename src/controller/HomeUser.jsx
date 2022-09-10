4eca6a20831a3bb9d6eb25e0b925c144c39eafbf
Osvaldo Angel <211125@ids.upchiapas.edu.mx>
2022-09-10 01:04:39 -0500

    taxi

--- src/controller/HomeUser.jsx
+++ src/controller/HomeUser.jsx
@@ -1,33 +1,78 @@
 import "../style/Style.css";
-import call from "../Img/llamada.png"
-import profail from "../Img/logo.png"
+import call from "../Img/llamada.png";
+import profail from "../Img/perfil.png";
+
 const HomeUser = () => {
   return(
       <div>
-          <h1 className="h1">safeTrasport</h1>
-          <h2 >Users</h2>
-          <img className="profile" src={profail}/>
+          <form className="from">
+
+              <div className="user">
+                  <img className="derecha profile" src={profail}/>
+
+                      <aside> <h1 className="black text-bold-font" >SafeTrasport</h1>
+                      </aside>
+
+                  <h2 className="users text-bold-font" >Users</h2>
+              </div>
 
 
-          <form>
-              <div className="users">
-                  <img className="profile" src={profail}/>
-              <p className="clasificacion">
-                  <input id="radio1" type="radio" name="estrellas" value="5"/>
-                    <label htmlFor="radio1">★</label>
-                   <input id="radio2" type="radio" name="estrellas" value="4"/>
-                  <label htmlFor="radio2">★</label>
-             <input id="radio3" type="radio" name="estrellas" value="3"/>
-              <label htmlFor="radio3">★</label>
-             <input id="radio4" type="radio" name="estrellas" value="2"/>
-             <label htmlFor="radio4">★</label>
-              <input id="radio5" type="radio" name="estrellas" value="1"/>
-             <label htmlFor="radio5">★</label>
-              </p>
-                  </div>
-          </form>
+              <div className="taxistas ">
+                  <img className="izquierda profile"
+                       src={profail}/>
+                  <aside>
+                      <h6 className="black">Osvaldo Angel Hernandez Gutierrez</h6 >
+                      <h6 className="black-rgb">06:32pm aug 09</h6 >
+                      <div className="estrellitas">
 
-          <div className="barra-logo">
+
+
+                          <input id="radio1" type="radio" name="" value="5"/>
+                          <label htmlFor="radio1">★</label>
+                          <input id="radio2" type="radio" name="estrellas" value="4"/>
+                          <label htmlFor="radio2">★</label>
+                          <input id="radio3" type="radio" name="estrellas" value="3"/>
+                          <label htmlFor="radio3">★</label>
+                          <input id="radio4" type="radio" name="estrellas" value="2"/>
+                          <label htmlFor="radio4">★</label>
+                          <input id="radio5" type="radio" name="estrellas" value="1"/>
+                          <label htmlFor="radio5">★</label>
+
+                      </div>
+                  </aside>
+
+                  <div className="reset"></div>
+              </div>
+              <div className="taxistas ">
+                  <img className="izquierda profile"
+                       src={profail}/>
+                  <aside>
+                      <h6 className="black">Osvaldo Angel Hernandez Gutierrez</h6 >
+                      <h6 className="black-rgb">06:32pm aug 09</h6 >
+                      <div className="estrellitas">
+
+
+
+                          <input id="radio1" type="radio" name="" value="5"/>
+                          <label htmlFor="radio1">★</label>
+                          <input id="radio2" type="radio" name="estrellas" value="4"/>
+                          <label htmlFor="radio2">★</label>
+                          <input id="radio3" type="radio" name="estrellas" value="3"/>
+                          <label htmlFor="radio3">★</label>
+                          <input id="radio4" type="radio" name="estrellas" value="2"/>
+                          <label htmlFor="radio4">★</label>
+                          <input id="radio5" type="radio" name="estrellas" value="1"/>
+                          <label htmlFor="radio5">★</label>
+
+                      </div>
+                  </aside>
+
+                  <div className="reset"></div>
+              </div>
+
+
+
+          <div className="barra-logo fotos">
 
           <svg  xmlns="http://www.w3.org/2000/svg" width="40" height="90" fill="currentColor"
                className="bi bi-house-door" viewBox="0 0 16 16">
@@ -46,8 +91,13 @@ const HomeUser = () => {
                   <path d="M12 9h2V8h-2v1Z"/>
               </svg>
 
-              <img src={call} className="call"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="90" fill="currentColor"
+                   className="bi bi-telephone-outbound" viewBox="0 0 16 16">
+                  <path
+                      d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"/>
+              </svg>
           </div>
+          </form>
       </div>
   )
 }
